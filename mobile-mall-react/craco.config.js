efd2cce12e697c997a2e6e507ac8c33837734425
zhengsb_oup <zhengsb_oup@si-tech.com.cn>
2022-08-11 11:33:18 +0800

    提交request

--- mobile-mall-react/craco.config.js
+++ mobile-mall-react/craco.config.js
@@ -8,5 +8,15 @@ module.exports = {
       // 约定：使用 @ 表示 src 文件所在路径
       '@': path.resolve(__dirname, 'src'),
     },
+    devServer: {
+      historyApiFallback: true,
+      port: '3000',
+      proxy: {
+        "/dev": {
+          target: "http://172.0.0.1:8081",
+          pathRewrite: { "^/dev": "" }
+        }
+      }
+    }
   },
 }
--- mobile-mall-react/src/utils/http.js
+++ mobile-mall-react/src/utils/http.js
@@ -1,4 +1,4 @@
-import http from './index'
+import http from './request'
  
  
 /**
